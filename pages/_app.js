f25adc6ba289588ab2489619f955323b464133d7
Cow <69002970+SilentElapid@users.noreply.github.com>
2021-10-24 21:46:07 +0000

    Improved typography and padding

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/style.css'
+import Head from 'next/head'
 
 function Aellica({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+		<>
+			<Head>
+				<link rel="shortcut icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default Aellica;
